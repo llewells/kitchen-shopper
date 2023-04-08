4aba61204d2db89214ee7553f920d191cc36bad8
Simon Llewellyn <llewells86@gmail.com>
2023-04-08 19:23:28 +0100

    CLASS TEMPLATES

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+class Recipe:
+    def __init__(self) -> None:
+        pass
+
+    def read_recipe():
+        return 
+
+
+class Kitchen:
+    def __init__(self) -> None:
+        pass
+def load_stock():
+    return 
+
+
+
 
 def main():
-    return "test"
+    return print("test")
 
 if __name__ == '__main__':
     main()
